f7d771f8014c7b844f4af471b98c0f0692b0820b
Riquelvi Contreras <riquelvicontreras@hotmail.com>
2017-02-01 17:41:13 -0500

    Add files via upload

---
+++ codeca06.py
@@ -0,0 +1,67 @@
+
+#quit = 0
+while quit != "q":
+
+    import random
+    print
+    print "Rolling Dice Game"
+    print
+    print "Press Enter to roll"
+
+    raw_input()
+    number = random.randint(1,6)
+
+    if number == 1:
+        print " _________________ "
+        print "|                 |"
+        print "|        O        |"
+        print "|                 |"
+        print "|_________________|"
+        print
+        print "Type 'q' to quit"
+        quit = raw_input()
+    if number == 2:
+        print " _________________ "
+        print "|                 |"
+        print "|   O         O   |"
+        print "|                 |"
+        print "|_________________|"
+        print
+        print "Type 'q' to quit"
+        quit = raw_input()
+    if number == 3:
+        print " _________________ "
+        print "|        O        |"
+        print "|                 |"
+        print "|  O           O  |"
+        print "|_________________|"
+        print
+        print "Type 'q' to quit"
+        quit = raw_input()
+    if number == 4:
+        print " _________________"
+        print "|  O            O |"
+        print "|                 |"
+        print "|  O            O |"
+        print "|_________________|"
+        print
+        print "Type 'q' to quit"
+        quit = raw_input()
+    if number == 5:
+        print " _________________"
+        print "| O             O |"
+        print "|        O        |"
+        print "| O             O |"
+        print "|_________________|"
+        print
+        print "Type 'q' to quit"
+        quit = raw_input()
+    if number == 6:
+        print " _________________ "
+        print "| O             O |"
+        print "| O             O |"
+        print "| O             O |"
+        print "|_________________|"
+        print
+        print "Type 'q' to quit"
+        quit = raw_input()
